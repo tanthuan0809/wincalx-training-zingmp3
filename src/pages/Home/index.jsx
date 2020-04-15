274a8bb0fcb9f565407a1a59856ea5a2edf8a8fc
CC02LAPTOP025 <dinhtai.nguyen@citynow.vn>
2020-04-15 08:11:39 +0700

    add-hot-songs-component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import React from "react";
 // components
 import HotSongBanners from "./HotSongBanners";
 import SongRanking from "./SongRanking";
-
+import HotSongs from "./HotSongs";
 /**
  * Home - home page
  */
@@ -14,6 +14,9 @@ const Home = () => {
     <div className="home">
       <HotSongBanners />
       <SongRanking />
+      <section>
+        <HotSongs />
+      </section>
     </div>
   );
 };
