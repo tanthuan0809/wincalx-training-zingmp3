2751cc044cd8fe64b1522fe1825b42b97463f6bd
CC02LAPTOP025 <dinhtai.nguyen@citynow.vn>
2020-04-15 08:14:29 +0700

    add-hot-theme-component

---
+++ src/pages/Home/HotTheme/main/HotThemeTitle/index.jsx
@@ -0,0 +1,18 @@
+// libs import
+import React from "react";
+
+// components
+
+// style
+import "./style.scss";
+
+/**
+ * HotThemeTitle - description title of hot theme component
+ * @param { string } title // title of hot theme component
+ */
+
+const HotThemeTitle = ({ title }) => {
+  return <div className="hot-theme-title-wrapper">{title}</div>;
+};
+
+export default HotThemeTitle;
