5b85a8b2277ba64e0a61aec934582cf6b5e68054
Truong Tan Thuan <tanthuan.truong@citynow.vn>
2020-04-19 14:38:22 +0700

    Updates

---
+++ precache-manifest.2d8042c032f6577f55ea78331b4ca182.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37e0ed71ff166ee0807b5b4cca23125c",
+    "url": "/wincalx-training-zingmp3/index.html"
+  },
+  {
+    "revision": "62e3e3abaa79299abde1",
+    "url": "/wincalx-training-zingmp3/static/css/2.29c1ed91.chunk.css"
+  },
+  {
+    "revision": "8dd3d0d9d73cbc4f92b4",
+    "url": "/wincalx-training-zingmp3/static/css/main.9373d557.chunk.css"
+  },
+  {
+    "revision": "62e3e3abaa79299abde1",
+    "url": "/wincalx-training-zingmp3/static/js/2.3cac097f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wincalx-training-zingmp3/static/js/2.3cac097f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd3d0d9d73cbc4f92b4",
+    "url": "/wincalx-training-zingmp3/static/js/main.24d15a2d.chunk.js"
+  },
+  {
+    "revision": "6ce6091eef6557b3427b",
+    "url": "/wincalx-training-zingmp3/static/js/runtime-main.459b653c.js"
+  }
+]);
